20c679baac17adb43670dcb896dffd49fffc0fbc
Shounak <shounak.b95@gmail.com>
2020-07-14 21:25:00 -0700

    initial commit

---
+++ app/src/main/java/com/example/onlinebookstrore/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.onlinebookstrore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    }
